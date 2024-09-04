0608d3770d94835c43a72afd531502c8fb36b879
Alexandru Zaharia <alex@igamaapps.com>
2024-09-04 10:58:51 +0300

    build: add initial project files

---
+++ Merch8x8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Merch8x8
+//
+//  Created by Alex Zaharia on 04.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Merch8x8/Merch8x8App.swift
@@ -0,0 +1,17 @@
+//
+//  Merch8x8App.swift
+//  Merch8x8
+//
+//  Created by Alex Zaharia on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Merch8x8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
